33978fed3683460565f99dda19d5b5f4a960daee
yhsooi <didgmltn7769@naver.com>
2022-01-12 03:02:53 +0900

    test insert querybuilder vs save basic options

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { createConnection } from 'typeorm';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getTest(options: string) {
+    try {
+      const users: User[] = [];
+      for (let i = 1; i <= 1000; i++) {
+        const user = new User();
+        user.username = `username`;
+        user.password = `password`;
+        user.name = 'test';
+        user.nickname = 'nickname';
+        user.email = 'user@example.com';
+        users.push(user);
+      }
+      if (options === 'query') {
+        console.time('queryBuilder');
+        await this.usersRepository
+          .createQueryBuilder('user')
+          .insert()
+          .into(User)
+          .values(users)
+          .execute();
+        console.timeEnd('queryBuilder');
+      } else {
+        console.time('basic');
+        await this.usersRepository.save(users);
+        console.timeEnd('basic');
+      }
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
---
+++ src/user.entity.ts
@@ -0,0 +1,37 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 20,
+  })
+  username: string;
+
+  @Column({
+    type: 'varchar',
+    length: 100,
+  })
+  password: string;
+
+  @Column({
+    type: 'varchar',
+    length: 10,
+  })
+  name: string;
+
+  @Column({
+    type: 'varchar',
+    length: 20,
+  })
+  nickname: string;
+
+  @Column({
+    type: 'varchar',
+    length: 50,
+  })
+  email: string;
+}
